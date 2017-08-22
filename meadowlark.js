e45ee1ed5d68548d70483f437c0766be0df2e7d1
qus1225 <qus1225@gmail.com>
2017-08-22 11:46:23 +0900

    Modulize GetFortune

--- meadowlark.js
+++ meadowlark.js
@@ -2,14 +2,8 @@ var express = require('express');
 
 var app =  express();
 
-// test data
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple."
-];
+var fortune = require('./lib/fortune');
+
 // Set Static Middleware
 app.use(express.static(__dirname + '/public'));
 
@@ -26,8 +20,7 @@ app
     res.render('home');
   })
   .get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
   });
 
 // custom 404 page
