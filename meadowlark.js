fcc12a55ce4acd7297262929f9d5b74f72199d73
qus1225 <qus1225@gmail.com>
2017-08-22 11:06:44 +0900

    Deliver Data from Router to View

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,15 @@ var express = require('express');
 
 var app =  express();
 
+// test data
+var fortunes = [
+  "Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple."
+];
+
 // Set Handlebar ViewEngine
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
 app.engine('hbs', handlebars.engine);
@@ -18,7 +27,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-  res.render('about');
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', {fortune: randomFortune});
 });
 
 // custom 404 page
