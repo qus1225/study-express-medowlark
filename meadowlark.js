5739ff861afd853214ac30b0f3fc8ac0a5d75e7e
qus1225 <qus1225@gmail.com>
2017-08-23 10:32:14 +0900

    Set BodyParser Middleware as PostRequestParser

--- meadowlark.js
+++ meadowlark.js
@@ -7,6 +7,8 @@ var fortune = require('./lib/fortune');
 // Set Static Middleware
 app.use(express.static(__dirname + '/public'));
 
+app.use(require('body-parser').urlencoded({ extended: true }));
+
 app.set('port', process.env.PORT || 3000);
 
 // Set Handlebar ViewEngine
@@ -36,6 +38,20 @@ app
   })
   .get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
+  })
+  .get('/newsletter', function (req, res) {
+    // use fake csrf value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+  })
+  .post('/process', function (req, res) {
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
+  })
+  .get('/thank-you', function (req, res) {
+    res.render('thankyou');
   })
 ;
 
